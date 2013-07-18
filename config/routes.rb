7c6c901e86d56e9b668d07f4c6ba15ad1cf0a631
q45 <quintinsmith@icloud.com>
2013-07-18 11:19:26 -0600

    rails_apps_composer: controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Blaw::Application.routes.draw do
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
